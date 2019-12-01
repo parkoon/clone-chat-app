b58a514810a1ec7a2a2f9c354d072f058c3d3eed
parkoon <par_koon@naver.com>
2019-12-01 18:10:02 +0900

    fixed: redirect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 // Components
 import ChatList from "./components/ChatList";
@@ -11,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/list" />
+        </Route>
         <Route exact path="/list">
           <ChatList transition={"let-to-right"} />
         </Route>
